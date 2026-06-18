0f9bb6b012984a60c02ae0aa7c9cf8368ac6f6f2
janaa <jana.akash652@gmail.com>
2026-06-18 10:26:43 +0530

    implemented file persistence and edit function

--- Cli_based_notes_app.py
+++ Cli_based_notes_app.py
@@ -1,5 +1,15 @@
 
-notes = {}
+import json
+
+try:
+    with open("notes.json","r") as file:
+        notes = json.load(file)
+except:
+    notes = {}
+
+def print_note_keys():
+    for i in notes.keys():
+        print(i)
 
 def add_note():
     title = input("Enter note title: ")
@@ -11,25 +21,43 @@ def add_note():
     notes[title] = note
 
 def view_note():
-    print(notes.keys())
-    cnf = input("Do you want to open a note? (y/n):")
-    if cnf == "y":
-        view_title = input("Enter title: ")
-        print(notes.get(view_title))
+    if notes == {}:
+        print("No notes found\n")
     else:
-        pass
+        print_note_keys()
+        cnf = input("Do you want to open a note? (y/n):")
+        if cnf == "y":
+            view_title = input("Enter title: ")
+            print(notes.get(view_title))
+        else:
+            pass
 
 def delete_note():
-    print(notes.keys())
+    print_note_keys()
     pop_title = input("Enter title of note to delete it: ")
     cnf = input("Are you sure? (y/n): ")
     if cnf == "y":
         notes.pop(pop_title)
+        print(f"Note with title {pop_title} deleted")
     else:
         print("Note not deleted.")
 
+def edit_note():
+    print_note_keys()
+    edit_title = input("Enter title of note to edit it: ")
+    cnf = input("Are you sure? (y/n): ")
+    if cnf == "y":
+        new_note = input("Enter new note: ")
+        notes[edit_title] = new_note
+    else:
+        print("Note not edit.")
+
+def json_save():
+    with open("notes.json","w") as file:
+        json.dump(notes,file)
+
 while True:
-    print("1. \nAdd note\n")
+    print("\n1. Add note\n")
     print("2. View note\n")
     print("3. Delete note\n")
     print("4. Edit note\n")
@@ -38,14 +66,19 @@ while True:
 
     if num == "1":
         add_note()
+        json_save()
 
     elif num == "2":
         view_note()
 
     elif num == "3":
         delete_note()
+        json_save()
+
     elif num == "4":
-        pass
+        edit_note()
+        json_save()
+
     elif num == "5":
         break
     else:
